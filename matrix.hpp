508576b2a3297e5b7d723a986c52f683cc06e624
EnrayMaker <250-5555@mail.ru>
2026-03-13 11:13:43 +0400

    Complete class Matrix

--- matrix.hpp
+++ matrix.hpp
@@ -32,4 +32,27 @@ public:
     }
   }
 
-}
+  T& operator()(size_t i, size_t j) { return arr[i * N + j]; }
+  const T& operator()(size_t i, size_t j) const { return arr[i * N + j]; }
+  size_t size() const { return N; }
+
+  Matrix<T> operator*(const Matrix<T>& m) const {
+    Matrix<T> res(N);
+    for (size_t i = 0; i < N; i++) {
+      for (size_t k = 0; k < N; k++) {
+        for (size_t j = 0; j < N; j++) {
+          res(i, j) += (*this)(i, k) * m(k, j);
+        }
+      }
+    }
+    return res;
+  }
+
+  friend std::ostream& operator<<(std::ostream& os, const Matrix<T>& m) {
+    for (size_t i = 0; i < m.N; i++) {
+      for (size_t j = 0; j < m.N; j++) os << m(i, j) << " ";
+      os << "\n";
+    }
+    return os;
+  }
+};
