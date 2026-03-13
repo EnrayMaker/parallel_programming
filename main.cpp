57a1133c6fe08fb4f7be92cb2967aab2b0243357
EnrayMaker <250-5555@mail.ru>
2026-03-13 11:03:20 +0400

    Add class Matrix

---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <algorithm>
+#include <random>
+
+using namespave std;
+
+tempalate <typename T>
+class Matrix {
+  std::vector<T> arr;
+  size_t N;
+
+public:
+  Matrix(size_t size) : N(size), arr(size * size, 0) {}
+
+  //Чтение из файла
+  void loadFromFile(const std::string& filename) {
+    std::ifstream in(filename);
+    for (auto& val : arr) in >> val;
+  }
+
+  // Заполнение значениями
+  void fillRandom(T min, T max, unsigned int seed) {
+    std::mt19937 gen(seed);
+    if constexpr (std::is_integral_v<T>) {
+      std::uniform_int_distribution<T> dist(min, max);
+      for (auto& val : arr) val = dist(gen);
+    } else {
+      std::uniform_real_distribution<T> dist(min, max);
+      for (auto& val : arr) val = dist(gen);
+    }
+  }
+}
