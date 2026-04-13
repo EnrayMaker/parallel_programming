599016c7f2daf5a80591b15cc1105e5cf84c5228
EnrayMaker <250-5555@mail.ru>
2026-04-13 17:19:42 +0400

    add cuda file

---
+++ lab4/kernel.cu
@@ -0,0 +1,39 @@
+#include <cuda_runtime.h>
+#include <iostream>
+
+// Ядро CUDA 
+global void matrixMulKernel(const int* A, const int* B, int* C, int N) {
+    int row = blockIdx.y * blockDim.y + threadIdx.y;
+    int col = blockIdx.x * blockDim.x + threadIdx.x;
+
+    if (row < N && col < N) {
+        int sum = 0;
+        for (int i = 0; i < N; i++) {
+            sum += A[row * N + i] * B[i * N + col];
+        }
+        C[row * N + col] = sum;
+    }
+}
+
+void runCudaMultiply(const int* h_A, const int* h_B, int* h_C, int N, int blockSize) {
+    int size = N * N * sizeof(int);
+    int *d_A, *d_B, *d_C;
+
+    cudaMalloc(&d_A, size);
+    cudaMalloc(&d_B, size);
+    cudaMalloc(&d_C, size);
+
+    cudaMemcpy(d_A, h_A, size, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_B, h_B, size, cudaMemcpyHostToDevice);
+
+    dim3 threadsPerBlock(blockSize, blockSize);
+    dim3 blocksPerGrid((N + blockSize - 1) / blockSize, (N + blockSize - 1) / blockSize);
+
+    matrixMulKernel<<<blocksPerGrid, threadsPerBlock>>>(d_A, d_B, d_C, N);
+
+    cudaMemcpy(h_C, d_C, size, cudaMemcpyDeviceToHost);
+
+    cudaFree(d_A);
+    cudaFree(d_B);
+    cudaFree(d_C);
+}
